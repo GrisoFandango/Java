420a85dcbb61cafb2d6386d5306c9dd467e1e017
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2019-09-30 16:46:07 +0100

    Add files via upload

---
+++ HiWorld/src/week3/Q1.java
@@ -0,0 +1,21 @@
+package week3;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// use of +,-,*,/,%
+		
+		
+				double num1=24.0;
+				double num2=5.0;
+				
+				System.out.println("Adding num1 + num2= "+(num1+num2));
+				System.out.println("Subtracting num1 - num2= "+(num1-num2));
+				System.out.println("Multiplying num1 * num2= "+(num1*num2));
+				System.out.println("Division of num1 by num2= "+(num1/num2));
+				System.out.println("Remainder num1 + num2= "+(num1%num2));		
+
+
+	}
+
+}
