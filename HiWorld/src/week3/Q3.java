40cc32bf587ecb6f9447d1f1085fc04e86aac989
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2019-10-01 11:09:48 +0100

    Add files via upload

--- HiWorld/src/week3/Q3.java
+++ HiWorld/src/week3/Q3.java
@@ -5,29 +5,37 @@ public class Q3
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		int ct=0,n=0,i=1,j=1;  
-		while(n<20)  
-		{  
-			j=1;  
-			ct=0;  
+	int num=1;//number to check if is prime
+	//int check=2;//number to use to divide num and check if is prime
+	int prime=0;// 1 is prime 0 is not
+	int n=0;//counter for the while loop
+	int sum=0;//variable to store the sum
+	//int x;
+	System.out.print("The first 10 prime number are: ");
+	while (n<=10) {
+		num++;
+		prime=1;
+		for ( int x = 2; x < num && prime==1; x++){
+			if (num % x == 0) 
+			prime=0;
+		}
+		if (prime==1) {
+			sum=sum+num;
+			n++;
+			System.out.print(num + " ");
+		}	
 			
-			while(j<=i)  
-			{  
-				if(i%j==0) 
-					ct++;  
-					j++;
-				
-			}  
-			if(ct==2)  
-			{  
-				System.out.printf("%d ",i);  
-				n++;  
-			}  
-		i++;  
-		}  
-	}  
-		  
+		}
+		
+	System.out.println("\nThe Sum of the first 10 prime numbers is: "+sum);	
+	}
+
+		
+		
+		
+		
+		
+	}
 	
-}
+
 
